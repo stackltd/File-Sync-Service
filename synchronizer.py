54930bfa449483d34b56e132bb7b715ef20ca10f
Evgeny Fedorov <stackltd@yandex.ru>
2024-02-16 01:15:59 +0700

    code refactoring

--- synchronizer.py
+++ synchronizer.py
@@ -4,7 +4,6 @@ import time
 import datetime as dt
 from datetime import datetime
 
-from requests.exceptions import ConnectionError
 from dotenv import load_dotenv, find_dotenv
 from loguru import logger
 
@@ -18,7 +17,6 @@ def main():
     """
     all_files = []
     start = True
-    stop = False
     is_changed = False
     errors = False
 
@@ -31,85 +29,107 @@ def main():
     uploader = UploaderToCloud(path_source=path_source, path_dist=path_dist)
     time_reload = 5
     data_cloud_1 = datetime.now()
-    cloud_scan_time_delta = 50
+    cloud_scan_time_delta = 10
 
     logger.remove()
     format_out = "{module} <green>{time:YYYY-MM-DD HH:mm:ss,SSS}</green> {level} <level>{message}</level>"
     logger.add(sys.stdout, format=format_out, level="INFO", colorize=True)
     logger.add(path_to_log, format=format_out, level="INFO")
 
+    def initializing(start_program, data_time, error_check):
+        """
+        Функция получения данных из облака при старте, возникновении ошибках, или по истечении таймаута для облака.
+        :param start_program: Флаг старта программы
+        :param data_time: начальная дата для отсчета таймаута контроля папки облака
+        :param error_check: флаг появления ошибки
+        :return:
+        """
+        data_cloud_2 = datetime.now()
+        data_cloud_delta = (data_cloud_2 - data_time).seconds
+        os.chdir(path_source)
+        if start_program or data_cloud_delta > cloud_scan_time_delta or error_check:
+            uploader.cloud_info = uploader.get_info()
+            if not start_program:
+                data_time = datetime.now()
+            if uploader.cloud_info is not None:
+                message = f"Синхронизация включена. Период сканирования исходной папки: {timeout}c, " \
+                          f"облака: не чаще {cloud_scan_time_delta}c."
+                if start_program and not error_check:
+                    logger.info(message)
+                start_program = False
+                if error_check:
+                    logger.info(f"Соединение восстановлено. {message}")
+
+                error_check = False
+
+        return start_program, data_time, error_check
+
+    def control_local_folder(check_changed):
+        for file in os.listdir():
+            # Отключение слежения, если создан файл "0"
+            if os.path.isfile(file) and file == "0":
+                os.remove("0")
+                logger.info(f"Синхронизация отключена.")
+                return
+            elif os.path.isfile(file):
+                all_files.append(file)
+                data_change_source = datetime.fromtimestamp(os.path.getmtime(file))
+                # Загрузка файла в облако
+                if file not in uploader.cloud_info.keys() and not errors:
+                    is_loaded = uploader.load_file(file)
+                    if is_loaded:
+                        check_changed = True
+                        logger.info(f"Файл {file} загружен")
+                else:
+                    # Перезапись файл в облако, если в он был изменен
+                    delta_tz = abs(time.timezone)  # Добавляем разницу в часовом поясе к метаданным из облака
+                    delta = dt.timedelta(seconds=delta_tz)
+                    data_in = uploader.cloud_info[file][:-6]
+                    data_change_dist = dt.datetime.strptime(data_in, '%Y-%m-%dT%H:%M:%S') + delta
+                    is_changed_file = data_change_source > data_change_dist
+                    if is_changed_file:
+                        is_loaded = uploader.load_file(file, overwrite=True)
+                        if is_loaded:
+                            check_changed = True
+                            logger.info(f"Файл {file} перезаписан")
+        return check_changed
+
+    def finishing(check_changed, data_time, error_check):
+        if check_changed:
+            check_changed = False
+            data_time = datetime.now()
+            uploader.cloud_info = uploader.get_info()
+            if uploader.cloud_info is None:
+                error_check = True
+        all_files.clear()
+        time.sleep(timeout)
+        return check_changed, data_time, error_check
+
     while True:
         try:
-            data_cloud_2 = datetime.now()
-            data_cloud_delta = (data_cloud_2 - data_cloud_1).seconds
-            os.chdir(path_source)
-            # Получения данных из облака при старте, возникновении ошибках, или по истечении таймаута для облака
-            if start or data_cloud_delta > cloud_scan_time_delta or errors:
-                uploader.cloud_info = uploader.get_info()
-                if not start:
-                    data_cloud_1 = datetime.now()
-                if uploader.cloud_info is not None:
-                    message = f"Синхронизация включена. Период сканирования исходной папки: {timeout}c, " \
-                              f"облака: не чаще {cloud_scan_time_delta}c."
-                    if start and not errors:
-                        logger.info(message)
-                    start = False
-                    if errors:
-                        logger.info(f"Соединение восстановлено. {message}")
-
-                    errors = False
-                else:
-                    raise ConnectionError
-
-            for file in os.listdir():
-                # Отключение слежения, если создан файл "0"
-                if os.path.isfile(file) and file == "0":
-                    os.remove("0")
-                    logger.info(f"Синхронизация отключена.")
-                    stop = True
-                    break
-                elif os.path.isfile(file):
-                    all_files.append(file)
-                    data_change_source = datetime.fromtimestamp(os.path.getmtime(file))
-                    # Загрузка файла в облако
-                    if file not in uploader.cloud_info.keys() and not errors:
-                        uploader.load(file)
-                        is_changed = True
-                        logger.info(f"Файл {file} загружен")
-                    else:
-                        # Перезапись файл в облако, если в он был изменен
-                        delta_tz = abs(time.timezone)  # Добавляем разницу в часовом поясе к метаданным из облака
-                        delta = dt.timedelta(seconds=delta_tz)
-                        data_in = uploader.cloud_info[file][:-6]
-                        data_change_dist = dt.datetime.strptime(data_in, '%Y-%m-%dT%H:%M:%S') + delta
-                        is_changed_file = data_change_source > data_change_dist
-                        if is_changed_file:
-                            uploader.load(file)
-                            is_changed = True
-                            logger.info(f"Файл {file} перезаписан")
-            if stop:
+            # Инициализация, получение данных с облака
+            start, data_cloud_1, errors = initializing(start_program=start, data_time=data_cloud_1, error_check=errors)
+            # контроль папки слежения, загрузка в облаков новых, или измененных файлов
+            result = control_local_folder(check_changed=is_changed)
+            if result is None:
                 break
-            # удаление файла из облака, если он отсутствует в папке слежения
-            for file in uploader.cloud_info:
-                if file not in all_files:
-                    uploader.delete(file)
-                    is_changed = True
-                    logger.info(f'Файл {file} удален')
-            # фиксация изменений
-            if is_changed:
-                is_changed = False
-                data_cloud_1 = datetime.now()
-                uploader.cloud_info = uploader.get_info()
+            else:
+                is_changed = result
+            # удаление посторонних файлов из облака
+            result = uploader.delete(list_local_folder=all_files)
+            if result is None:
+                errors = True
+            else:
+                is_changed = True
+            # завершение периода слежения, подготовка к следующему
+            is_changed, data_cloud_1, errors = finishing(is_changed, data_cloud_1, errors)
 
-            all_files.clear()
-            time.sleep(timeout)
-
-        except (FileNotFoundError, ConnectionError) as ex:
+        except (FileNotFoundError, AttributeError) as ex:
             info_error = ex.__repr__()
             errors = True
             if "FileNotFoundError" in info_error:
                 logger.error(f"{ex}. Неверно указан путь к локальной папке")
-            logger.info(f"Следующая попыка синхронизации через {time_reload} c.")
+            logger.info(f"Следующая попытка синхронизации через {time_reload} c.")
             time.sleep(time_reload)
 
 
--- uploader.py
+++ uploader.py
@@ -16,10 +16,11 @@ class UploaderToCloud:
         self.path_source = path_source
         self.path_dist = path_dist
         self.cloud_info = {}
+        self.file_for_delete = ""
 
     def get_info(self):
+        url = f'{BASE_URL}?path={self.path_dist}'
         try:
-            url = f'{BASE_URL}?path={self.path_dist}'
             result = requests.get(url=url, headers=HEADERS).json()
             meta_info = result["_embedded"]["items"]
             names = {info["name"]: info["modified"] for info in meta_info}
@@ -27,32 +28,43 @@ class UploaderToCloud:
         except KeyError:
             logger.error("Ошибка при получении данных с Яндекс-диска."
                          " Возможно, в облаке отсутствует указанная папка, или не найден токен")
-            return None
         except ConnectionError:
             logger.error("Ошибка при получении данных с Яндекс-диска. Не удается установить связь с облаком")
-            return None
 
-    def load(self, file_name) -> None:
+    def load_file(self, file_name, overwrite=False):
+        url = f'{BASE_URL}/upload?path={self.path_dist}/{file_name}&overwrite={overwrite}'
         try:
-            url = f'{BASE_URL}/upload?path={self.path_dist}/{file_name}&overwrite=True'
             result = requests.get(url=url, headers=HEADERS).json()
             path_to_local_file = os.path.join(self.path_source, file_name)
             with open(path_to_local_file, 'rb') as f:
                 requests.put(result['href'], files={'file': f})
-        except KeyError:
-            message = result["message"]
-            if message == "Не авторизован.":
-                logger.error(f"Не удалось загрузить файл. Ошибка авторизации. Проверьте ваш токен")
-            else:
-                logger.error(f"Не удалось загрузить файл. Ошибка: {message}")
+            return True
+        except (KeyError, ConnectionError) as ex:
+            info_error = ex.__repr__()
+            if "ConnectionError" in info_error and overwrite:
+                logger.error(f"Не удалось перезаписать файл {file_name}. Проверьте соединение.")
+            elif not overwrite:
+                logger.error(f"Не удалось загрузить файл {file_name}. Проверьте соединение.")
 
-    def delete(self, file_name) -> None:
+    def delete(self, list_local_folder):
+        """
+        Функция удаления файла из облака, если он отсутствует в папке слежения.
+        :param list_local_folder:
+        :return: При успешной работе функции возвращает True, иначе - False
+        """
         try:
-            url = f'{BASE_URL}?path={self.path_dist}/{file_name}'
-            requests.delete(url=url, headers=HEADERS)
+            check_delete = False
+            for file_name in self.cloud_info:
+                url = f'{BASE_URL}?path={self.path_dist}/{file_name}'
+                if file_name not in list_local_folder:
+                    self.file_for_delete = file_name
+                    requests.delete(url=url, headers=HEADERS)
+                    logger.info(f'Файл {file_name} удален')
+                    check_delete = True
+            return check_delete
         except ConnectionError:
-            logger.error("Ошибка соединения. Не удалось удалить файл.")
-
+            logger.error(f"Ошибка соединения. Не удалось удалить файл {self.file_for_delete}.")
+            return
 
 if __name__ == "__main__":
     pass
